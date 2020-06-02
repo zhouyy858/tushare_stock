9e92b2f3c3738daef6521ce76c1248b3232efa36
yuyuan.zhou <yuyuan.zhou@nio.com>
2020-06-02 17:41:13 +0800

    add sea turtle model

---
+++ src/02_sea_turtle_model.py
@@ -0,0 +1,12 @@
+import pandas as pd  
+import numpy as np
+import talib as ta
+from datetime import datetime,timedelta
+import matplotlib.pyplot as plt
+#正常显示画图时出现的中文和负号
+from pylab import mpl
+mpl.rcParams['font.sans-serif']=['SimHei']#设置字体
+mpl.rcParams['axes.unicode_minus']=False #显示中文
+
+
+
